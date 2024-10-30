6ea47c8bc394376a44c10924a7418204b1501d2a
Pelumi Amao <113313095+Pekumiiix@users.noreply.github.com>
2024-10-30 20:25:30 +0100

    Update checkout-tab.tsx

--- app/checkout/sections/checkout-tab.tsx
+++ app/checkout/sections/checkout-tab.tsx
@@ -1,4 +1,4 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsContent, TabsList } from "@/components/ui/tabs";
 import { CustomTabTriggers } from "@/app/[productId]/sections/product-tabs";
 import DeliveryTab from "./delivery-tab";
 
